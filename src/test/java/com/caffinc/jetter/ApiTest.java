855fb0ab0efee067ef63b970330807901b14de41
Sriram Keerthi Madhava Kunjathur <Sriram Keerthi Madhava Kunjathur>
2016-05-13 18:09:40 +0530

    Starting testing

---
+++ src/test/java/com/caffinc/jetter/ApiTest.java
@@ -0,0 +1,16 @@
+package com.caffinc.jetter;
+
+import org.junit.Test;
+
+/**
+ * Tests the API class
+ *
+ * @author Sriram
+ * @since 4/28/2016
+ */
+public class ApiTest {
+    @Test
+    public static void testApi() {
+
+    }
+}
---
+++ src/test/java/com/caffinc/jetter/DummyResource.java
@@ -0,0 +1,8 @@
+package com.caffinc.jetter;
+
+/**
+ * @author Sriram
+ * @since 4/28/2016
+ */
+public class DummyResource {
+}
