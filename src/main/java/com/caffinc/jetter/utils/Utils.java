6fa0563c589cc6b111180f7ff09e1e4f993fad98
SriramKeerthi <shramk@gmail.com>
2016-12-10 12:22:36 -0800

    Adding a few tests

--- src/main/java/com/caffinc/jetter/utils/Utils.java
+++ src/main/java/com/caffinc/jetter/utils/Utils.java
@@ -1,6 +1,8 @@
 package com.caffinc.jetter.utils;
 
 /**
+ * Utility methods
+ *
  * @author Sriram
  * @since 4/26/2016
  */
@@ -16,6 +18,5 @@ public class Utils {
     public static <T> T getOrDefault(T property, T defaultVal) {
         return property != null ? property : defaultVal;
     }
-
 
 }
